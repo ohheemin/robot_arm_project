d0f789ca3021a60253d5ef433ce4251103aa72df
Wonho Yun <ywh@robotis.com>
2025-03-13 15:30:06 +0900

    CI
    Signed-off-by: Wonho Yun <ywh@robotis.com>

--- ros/dynamixel_sdk_examples/src/read_write_node.py
+++ ros/dynamixel_sdk_examples/src/read_write_node.py
@@ -26,22 +26,23 @@ from dynamixel_sdk_custom_interfaces.msg import SetPosition
 from dynamixel_sdk_custom_interfaces.srv import GetPosition
 
 # Control table address
-ADDR_OPERATING_MODE = 11 # Control table address is different in Dynamixel model
+ADDR_OPERATING_MODE = 11  # Control table address is different in Dynamixel model
 ADDR_TORQUE_ENABLE = 64
 ADDR_GOAL_POSITION = 116
 ADDR_PRESENT_POSITION = 132
 
 # Protocol version
-PROTOCOL_VERSION = 2.0 # Default Protocol version of DYNAMIXEL X series.
+PROTOCOL_VERSION = 2.0  # Default Protocol version of DYNAMIXEL X series.
 
 # Default settings
-DXL_ID = 1 # Dynamixel ID : 1
-BAUDRATE = 57600 # Dynamixel default baudrate : 57600
-DEVICE_NAME = "/dev/ttyUSB0" # Check which port is being used on your controller
+DXL_ID = 1  # Dynamixel ID : 1
+BAUDRATE = 57600  # Dynamixel default baudrate : 57600
+DEVICE_NAME = "/dev/ttyUSB0"  # Check which port is being used on your controller
 
-TORQUE_ENABLE = 1 # Value for enabling the torque
-TORQUE_DISABLE = 0 # Value for disabling the torque
-POSITION_CONTROL = 3 # Value for position control mode
+TORQUE_ENABLE = 1  # Value for enabling the torque
+TORQUE_DISABLE = 0  # Value for disabling the torque
+POSITION_CONTROL = 3  # Value for position control mode
+
 
 class ReadWriteNode(Node):
     def __init__(self):
