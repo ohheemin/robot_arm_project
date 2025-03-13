711c0bb2a340857277e413b9e1943d9c677687ac
Wonho Yun <ywh@robotis.com>
2025-03-13 13:34:10 +0900

    Modified author info
    Signed-off-by: Wonho Yun <ywh@robotis.com>

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,15 @@
 from setuptools import setup, find_packages
 import platform
 
+
+authors_info = [
+    ('Leon Jung', 'rwjung@robotis.com'),
+    ('Wonho Yun', 'ywh@robotis.com'),
+]
+
+authors = ', '.join(author for author, _ in authors_info)
+author_emails = ', '.join(email for _, email in authors_info)
+
 setup(
     name='dynamixel_sdk',
     version='3.8.2',
@@ -11,9 +20,7 @@ setup(
     description='Dynamixel SDK 3. python package',
     long_description=open('README.txt').read(),
     url='https://github.com/ROBOTIS-GIT/DynamixelSDK',
-    author='Leon Jung',
-    author_email='rwjung@robotis.com',
-    author='Wonho Yun',
-    author_email='ywh@robotis.com',
+    author=authors,
+    author_email=author_emails,
     install_requires=['pyserial']
 )
